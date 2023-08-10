7ff8dd72d79872220f1feae52c635d3a73263c1e
mbeekman <mbeekman@gmail.com>
2023-08-10 00:04:15 -0500

    Fleshing out back end service

---
+++ BackEnd2/Controllers/BackEnd2Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd2;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BackEnd2Controller : ControllerBase
+{
+    private const int BackEnd2Result = 117;
+
+    [HttpGet]
+    public int GetBackEndResult()
+    {
+        return BackEnd2Result;
+    }
+}
